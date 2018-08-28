336d2fd909f76628b68786a4ad13f8410b5a7bde
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2018-08-27 23:26:02 -0400

    edit: updates middleware

--- server/index.js
+++ server/index.js
@@ -2,10 +2,9 @@
 
 const express = require(`express`)
 const volleyball = require(`volleyball`)
-const bodyParser = require(`body-parser`)
 const path = require(`path`)
 
-const { db } = require(`./db`)
+const {db} = require(`./db`)
 
 const app = express()
 
@@ -13,8 +12,8 @@ const app = express()
 app.use(volleyball)
 
 // Body parsing middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
 // Static middleware
 app.use(express.static(path.join(__dirname, `../public`)))
@@ -24,14 +23,14 @@ app.use(`/api`, require(`./api`))
 
 // All other requests
 app.get(`*`, (req, res) => {
-	res.sendFile(path.join(__dirname, `../public`))
+  res.sendFile(path.join(__dirname, `../public`))
 })
 
 // Error handling
 app.use((error, req, res, next) => {
-	console.error(error)
-	console.error(error.stack)
-	res.status(error.status || 500).send(error.message || `Internal Server Error`)
+  console.error(error)
+  console.error(error.stack)
+  res.status(error.status || 500).send(error.message || `Internal Server Error`)
 })
 
 // Define port
@@ -39,7 +38,7 @@ const PORT = process.env.PORT || 1337
 
 // Sync the DB and start up the server
 db.sync(/* {force : true} */)
-	.then(() => {
-		console.log(`\nDatabase Synced\n`)
-		app.listen(PORT, () => console.log(`Partying hard on http://localhost:${PORT}\n`))
-	})
+  .then(() => {
+    console.log(`\nDatabase Synced\n`)
+    app.listen(PORT, () => console.log(`Partying hard on http://localhost:${PORT}\n`))
+  })
